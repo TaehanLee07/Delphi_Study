230b014c91a8873146c297eef6b6e5cf2a91df6f
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-10-25 13:33:56 +0900

    Create code.pas

---
+++ sample_projects/code.pas
@@ -0,0 +1,37 @@
+unit simple_program;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Edit1: TEdit;
+    procedure Button1Click(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  if Edit1.Text = '곽권' then
+    ShowMessage('삼일연애님 안녕하세요')
+  else
+    ShowMessage(Edit1.Text + '님 안녕하세요');
+end;
+
+
+
+end.
